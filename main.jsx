0982c92c97a8eff20310f955c5b89276d39171f7
wassim190 <wassimbounecer1113@gmail.com>
2025-10-19 21:17:55 +0100

    Initialize React app with welcome message

---
+++ main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>مرحبًا بك في Ginos Anime 🎬</h1>
+      <p>الموقع يعمل بنجاح!</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
